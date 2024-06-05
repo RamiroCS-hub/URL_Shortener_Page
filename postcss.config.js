297ae5ec4ea91b04ffae5568650a671b10266961
luciocape <luciodimascapezzuto@gmail.com>
2024-06-04 23:03:40 -0300

    Organice el workspace con todas las tecnologías necesarias para empezar a trabajar (Vite, React, Tailwind y SASS)

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+
+export default {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ],
+}
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+function App() {
+	const styles = {
+		height: "500px",
+	}
+	return (
+		<>
+			<div className="text-center w-full" style={styles}>
+				hello world
+			</div>
+		</>
+	);
+}
+
+export default App;
