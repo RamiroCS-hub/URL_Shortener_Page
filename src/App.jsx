321e30f52416c88131ca7a6593c2e612043e82aa
luciocape <luciodimascapezzuto@gmail.com>
2024-06-13 23:57:22 -0300

    Restructuración y rediseño, estilos generales de la homepage terminados

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,82 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.scss";
+import { ArrowDown } from "./iconos/arrow-down";
+import { ArrowRight } from "./iconos/arrow-right";
 
 function App() {
 	const beneficios = [
-		"Ver URLs acortadas y sus estadísticas",
-		"Crear URLs personalizadas",
-		"Proteger URLs con contraseña",
-		"Editar URLs acortadas",
+		{
+			value: "Personaliza tus URLs acortadas",
+			id: 1,
+		},
+		{
+			value: "Ve tus URLs y sus estadísticas",
+			id: 2,
+		},
+		{
+			value: "Protege tus URLs con contraseña",
+			id: 3,
+		},
+		{
+			value: "Edita tus URLs acortadas o borralas",
+			id: 4,
+		},
 	];
 	return (
 		<>
-			<header className="w-3/4 h-auto mt-6 mb-10 m-auto  border-4 border-red-700 text-center">
-				<h1>Url Shortener</h1>
-				<h2>
-					Acorta tus url <strong>RAPIDO</strong>,{" "}
-					<strong>GRATIS</strong> y <strong>SEGURO</strong>
-				</h2>
-			</header>
-			<main className="w-3/4 m-auto border-4 border-violet-800">
-				<section className="register h-60 border-4 border-black flex shadow-slate-800">
-					<div className="w-5/6 h-5/6  m-auto border-4 border-yellow-500 flex justify-between align-middle">
-						<ul className="flex flex-col gap-5">
-							{beneficios.map((x, indice) => {
-								return (
-									<li
-										key={indice}
-										className="list-disc justify-between text-xl"
-									>
-										{x}
-									</li>
-								);
-							})}
+			<div className="doted-background"></div>
+			<main className="w-full flex flex-col items-center">
+				<section className="w-full h-screen flex flex-col justify-start items-center mb-2">
+					<h1 className="text-center justify-self-start mt-5">
+						Url Shortener
+					</h1>
+					<h2 className="text-center">
+						Acorta tus url <strong>RAPIDO</strong>,{" "}
+						<strong>GRATIS</strong> y <strong>SEGURO</strong>
+					</h2>
+
+					<div className="w-full h-full flex flex-col justify-center items-center">
+						<ul className="flex flex-col gap-6 mb-7 text-center text-2xl">
+							{beneficios.map((x) => (
+								<li
+									key={x.id}
+									className="justify-between"
+								>
+									{x.value}
+								</li>
+							))}
 						</ul>
-						<div className="">
-							<p className="text-blue-900 font-extrabold text-xl mb-3">
-								Obten los beneficios:
-							</p>
-							<span className="flex flex-col h-2/3 gap-3 justify-center">
-								<button className="w-44 text-xl p-2 bg-slate-900 rounded-2xl">
+						<div className="mt-2 w-auto flex justify-center gap-7 text-xl mb-20" >
+								<button className="p-2 bg-teal-500 border-4 border-teal-500 rounded-3xl">
 									Registrarse
 								</button>
-								<button className="w-44 text-xl p-2 bg-slate-900 rounded-2xl">
+								<button className="p-2 border-4 border-teal-500 rounded-3xl">
 									Iniciar seción
 								</button>
-							</span>
 						</div>
+						<a href="#shortener" className="link-arrow flex flex-col justify-center items-center font-bold "><ArrowDown clases='animate-bounce arrow' dimentions={50}/><span>Acortar url</span></a>
+						
 					</div>
 				</section>
-				<section className="w-5/6 h-48 m-auto border-4 border-blue-800 mt-5 mb-10">
-					<div className="border-4 border-gray-800 grid grid-cols-3">
-						<input className="h-14 rounded-xl" type="text" />
-						{
-							//flecha svg
-						}
-						<span></span>
-						<input className="h-14 rounded-xl" type="text" name="" id="" />
+				<section id="shortener" className="w-3/4 h-screen m-auto mt-3 flex flex-col justify-center items-center">
+					<div className="w-full flex justify-between">
+						<input className="h-16 w-2/5 rounded-xl bg-neutral-300" type="text" />
+						<span className="flex items-center text-white ">
+							<ArrowRight clases='arrow text-cyan-300' dimentions={60}/>
+						</span>
+						<input
+							className="h-16 w-2/5 rounded-xl bg-neutral-300 text-cyan-300"
+							type="text"
+							name=""
+							id=""
+						/>
 					</div>
 
-					<input className="w-full mt-6" type="text" />
+					<input
+						className="w-full h-16 mt-4 rounded-xl bg-neutral-300"
+						type="text"
+					/>
 				</section>
 			</main>
 		</>
