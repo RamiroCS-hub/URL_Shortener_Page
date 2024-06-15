d3daca66f600c35d5ac5e0e75da50f0619c2ddb8
luciocape <luciodimascapezzuto@gmail.com>
2024-06-14 23:26:05 -0300

    Boton de acortar estilizado y otros cambios menores

--- src/App.jsx
+++ src/App.jsx
@@ -40,43 +40,58 @@ function App() {
 					<div className="w-full h-full flex flex-col justify-center items-center">
 						<ul className="flex flex-col gap-6 mb-7 text-center text-2xl">
 							{beneficios.map((x) => (
-								<li
-									key={x.id}
-									className="justify-between"
-								>
+								<li key={x.id} className="justify-between">
 									{x.value}
 								</li>
 							))}
 						</ul>
-						<div className="mt-2 w-auto flex justify-center gap-7 text-xl mb-20" >
-								<button className="p-2 bg-teal-500 border-4 border-teal-500 rounded-3xl">
-									Registrarse
-								</button>
-								<button className="p-2 border-4 border-teal-500 rounded-3xl">
-									Iniciar seción
-								</button>
+						<div className="mt-2 w-auto flex justify-center gap-7 text-xl mb-20">
+							<button className="p-2 bg-teal-500 border-4 border-teal-500 rounded-3xl">
+								Registrarse
+							</button>
+							<button className="p-2 border-4 border-teal-500 rounded-3xl bg-transparent bg-gradient-to-t hover:from-emerald-300 hover:to-teal-200 transition ease-linear duration-200">
+								Iniciar sesión
+							</button>
 						</div>
-						<a href="#shortener" className="link-arrow flex flex-col justify-center items-center font-bold "><ArrowDown clases='animate-bounce arrow' dimentions={50}/><span>Acortar url</span></a>
-						
+						<a
+							href="#shortener"
+							className="link-arrow flex flex-col justify-center items-center font-bold "
+						>
+							<ArrowDown
+								clases="animate-bounce arrow"
+								dimentions={50}
+							/>
+							<span>Acortar url</span>
+						</a>
 					</div>
 				</section>
-				<section id="shortener" className="w-3/4 h-screen m-auto mt-3 flex flex-col justify-center items-center">
-					<div className="w-full flex justify-between">
-						<input className="h-16 w-2/5 rounded-xl bg-neutral-300" type="text" />
-						<span className="flex items-center text-white ">
-							<ArrowRight clases='arrow text-cyan-300' dimentions={60}/>
-						</span>
+				<section
+					id="shortener"
+					className="w-3/4 h-screen m-auto mt-3 flex flex-col justify-center items-center"
+				>
+					<div className="w-full flex justify-between gap-3">
 						<input
-							className="h-16 w-2/5 rounded-xl bg-neutral-300 text-cyan-300"
-							type="text"
+							className="input h-16 w-full rounded-xl bg-neutral-300 p-4"
+							type="url"
+							name=""
+							id=""
+						/>
+						<span className="flex items-center text-white w-36">
+							<ArrowRight clases="arrow text-cyan-300 w-full" />
+						</span>
+
+						<input
+							className="input h-16 w-full rounded-xl bg-neutral-300 p-4"
+							type="url"
 							name=""
 							id=""
 						/>
 					</div>
 
 					<input
-						className="w-full h-16 mt-4 rounded-xl bg-neutral-300"
-						type="text"
+						className="acortar w-full h-20 mt-5 rounded-xl bg-blue-950 text-4xl text-teal-500 font-bold cursor-pointer hover:bg-blue-900"
+						type="button"
+						value="Acortar"
 					/>
 				</section>
 			</main>
--- src/iconos/arrow-right.jsx
+++ src/iconos/arrow-right.jsx
@@ -3,8 +3,6 @@ export function ArrowRight({ clases, dimentions }) {
         <svg
         className={clases}
         xmlns="http://www.w3.org/2000/svg"
-        width={dimentions}
-        height={dimentions}
         viewBox="0 0 256 256"
     >
         <path
