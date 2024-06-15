20dc17dfce0ff662212a89c6b12ae77a28301bf0
Rama <ramirocarnicersouble8@gmail.com>
2024-06-15 14:49:52 -0300

    [feat] Edit pop component addded

---
+++ Url-Shortener-Page/src/components/EditPop.jsx
@@ -0,0 +1,9 @@
+export const EditPop = ({ data, handleEdit }) => {
+  return (
+    <>
+      <div>Se esta queriendo editar algo</div>
+      <button onClick={() => handleEdit()}>SAVE</button>
+    </>
+
+  )
+}
